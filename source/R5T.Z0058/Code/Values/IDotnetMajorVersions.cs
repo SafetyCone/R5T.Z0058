3fbad4047551b50e9d1aec366394d2fa01ff5cec
davidcoats <david@getsafetycone.com>
2023-11-05 11:53:23 -0800

    Interim changes.

--- source/R5T.Z0058/Code/Values/IDotnetMajorVersions.cs
+++ source/R5T.Z0058/Code/Values/IDotnetMajorVersions.cs
@@ -11,7 +11,9 @@ namespace R5T.Z0058
     public partial interface IDotnetMajorVersions : IValuesMarker
     {
         public IDotnetMajorVersion V2 => 2.ToDotnetMajorVersion();
+        public IDotnetMajorVersion V3 => 3.ToDotnetMajorVersion();
         public IDotnetMajorVersion V5 => 5.ToDotnetMajorVersion();
         public IDotnetMajorVersion V6 => 6.ToDotnetMajorVersion();
+        public IDotnetMajorVersion V7 => 7.ToDotnetMajorVersion();
     }
 }
